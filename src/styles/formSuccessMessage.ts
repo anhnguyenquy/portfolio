af5cbf41d0c1ccba1b0c5a743a8deff910a5f970
anhnguyenquy <anhnguyenquy2407@gmail.com>
2022-08-29 19:18:54 +0700

    formSuccessMessage fix

--- src/styles/formSuccessMessage.ts
+++ src/styles/formSuccessMessage.ts
@@ -1,5 +1,5 @@
-export const formErrorMessage = {
-  color: 'var(--chakra-colors-red-500)',
+export const formSuccessMessage = {
+  color: 'var(--chakra-colors-green-500)',
   marginTop: '0.5rem',
   fontSize: '0.875rem'
 }
